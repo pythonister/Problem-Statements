e9f1197e78666adde0f1f97596e4060410f137b5
Powerful Thoughts <55293114+powerfulthougths@users.noreply.github.com>
2019-09-16 19:45:59 +0530

    Update solution.py

--- DiwaharN/solution.py
+++ DiwaharN/solution.py
@@ -1,1 +1,12 @@
-print('hello world')
+start=int(input("Enter the starting value : "))
+end=int(input("Enter the ending value : "))
+l=[]
+for i in range(start,end):
+    if(i%7==0 and i%5!=0):
+        l.append(str(i))
+
+output=",".join(l) #Joining the list with comma-seperated join() fuction is used
+print(output)
+        
+
+ 
